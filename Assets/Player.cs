16ccdc18378c4cf1f888c6944d132b6cbd20b431
Chris Grych <ceggrych@gmail.com>
2019-10-17 22:15:50 -0500

    Adds prelim code

---
+++ Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int energy = 5;
+    [SerializeField] private float gpa = 3.0f;
+    [SerializeField] private int teamRank = 1000;
+    [SerializeField] private string rankCategory = rank.Bronze.ToString();
+
+    [SerializeField] private TextMeshProUGUI energyText;
+    [SerializeField] private TextMeshProUGUI gpaText;
+    [SerializeField] private TextMeshProUGUI teamRankText;
+
+    private enum rank { Bronze, Silver, Gold, Platinum, Diamond, Master}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateTextFields();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void UpdateTextFields()
+    {
+        energyText.text = energy.ToString();
+        gpaText.text = gpa.ToString();
+        teamRankText.text = teamRank.ToString();
+    }
+}
